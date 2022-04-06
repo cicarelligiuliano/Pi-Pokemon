0374feb54d2d5793c6bf21968dbda56b2817be3c
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-04-06 20:07:25 -0300

    Arreglando PORT

--- api/index.js
+++ api/index.js
@@ -3,8 +3,8 @@ const { conn, Pokemon, Type } = require("./src/db.js");
 const axios = require("axios").default;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-    server.listen(process.env.PORT, async () => {
+conn.sync({ force: false }).then(() => {
+    server.listen(process.env.PORT || 5000, async () => {
         // console.log("Servidor levantado en puerto 3001"); // eslint-disable-line no-console
         await axios
             .get("https://pokeapi.co/api/v2/type")
