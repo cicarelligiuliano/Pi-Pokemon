95d88af4353b54bb61ac0d7519885e6800229e59
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-04-06 22:21:48 -0300

    Force false

--- api/src/db.js
+++ api/src/db.js
@@ -33,6 +33,8 @@ let sequelize =
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
